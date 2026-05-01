1fac9287a109fd34dea9ca9b85eecb89be734c8f
Denis Castillón <orangeneko158@outlook.com>
2026-05-01 15:29:04 -0700

    inyectar lib

--- blinderkitten.nix
+++ blinderkitten.nix
@@ -8,21 +8,6 @@ in
   pkgs.buildFHSEnv {
     name = "blinderkitten";
 
-    # targetPkgs = pkgs:
-    #   [curl]
-    #   ++ (with pkgs; [
-    #     glib
-    #     zlib
-    #     openssl
-    #     stdenv.cc.cc.lib
-    #     alsa-lib # libasound.so.2
-    #     freetype # libfreetype.so.6
-    #     mesa # libGL.so.1
-    #     libglvnd # tambien libGL.so.1
-    #     curlWithGnuTls # libcurl-gnutls.so.4
-    #     avahi # libavahi-common.so.3
-    #   ]);
-
     targetPkgs = pkgs: [
       curl
       pkgs.glib
@@ -36,7 +21,9 @@ in
       pkgs.avahi
     ];
 
+    extraOutputsToInstall = ["lib"];
+
     runScript = ''
-      bash -c "export LD_LIBRARY_PATH=${curl}/lib:\$LD_LIBRARY_PATH; ./squashfs-root/AppRun"
+      ./squashfs-root/AppRun
     '';
   }
