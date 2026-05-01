237e39c776986756bf5eb8dd07d940b588bb1529
Denis Castillón <orangeneko158@outlook.com>
2026-05-01 14:37:22 -0700

    añadir más dependencias por cada error

--- blinderkitten.nix
+++ blinderkitten.nix
@@ -9,6 +9,10 @@ pkgs.buildFHSEnv {
       zlib
       openssl
       stdenv.cc.cc.lib
+      alsa-lib # libasound.so.2
+      freetype # libfreetype.so.6
+      mesa # libGL.so.1
+      libglvnd # tambien libGL.so.1
     ];
 
   runScript = ''
