7cbb9bcd7f6e5ea9ca7ecde67594109c15277629
Denis Castillón <orangeneko158@outlook.com>
2026-05-01 15:06:30 -0700

    formatear

--- blinderkitten.nix
+++ blinderkitten.nix
@@ -1,25 +1,24 @@
-{pkgs ? import <nixpkgs> {}}:
-let
+{pkgs ? import <nixpkgs> {}}: let
   curl = pkgs.curlWithGnuTls.out;
 in
-pkgs.buildFHSEnv {
-  name = "blinderkitten";
+  pkgs.buildFHSEnv {
+    name = "blinderkitten";
 
-  targetPkgs = pkgs:
-    with pkgs; [
-      curlWithGnuTls.out
-      glib
-      zlib
-      openssl
-      stdenv.cc.cc.lib
-      alsa-lib # libasound.so.2
-      freetype # libfreetype.so.6
-      mesa # libGL.so.1
-      libglvnd # tambien libGL.so.1
-      curlWithGnuTls # libcurl-gnutls.so.4
-    ];
+    targetPkgs = pkgs:
+      with pkgs; [
+        curlWithGnuTls.out
+        glib
+        zlib
+        openssl
+        stdenv.cc.cc.lib
+        alsa-lib # libasound.so.2
+        freetype # libfreetype.so.6
+        mesa # libGL.so.1
+        libglvnd # tambien libGL.so.1
+        curlWithGnuTls # libcurl-gnutls.so.4
+      ];
 
-  runScript = ''
-    bash -c "export LD_LIBRARY_PATH=${curl}/lib:\$LD_LIBRARY_PATH; ./squashfs-root/AppRun"
-  '';
-}
+    runScript = ''
+      bash -c "export LD_LIBRARY_PATH=${curl}/lib:\$LD_LIBRARY_PATH; ./squashfs-root/AppRun"
+    '';
+  }
